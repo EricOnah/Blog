645c59111eead22e17612fde939e523a7d199a3c
Eric Onah <ericdavid4u@gmail.com>
2024-11-16 22:12:21 +0100

    Updated app.js to pass posts array to home route, added note.txt, modified styles.css to position footer, and updated home.ejs to render posts array

--- app.js
+++ app.js
@@ -20,7 +20,10 @@ const contactContent =
   " Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis ratione officiis animi sequi quos reprehenderit quasi ex omnis maiores, assumenda molestiae, laudantium incidunt, obcaecati asperiores suscipit error rerum! ipsum dolor sit amet consectetur adipisicing elit. Reiciendis ratione officiis animi sequi quos reprehenderit quasi ex omnis maiores, assumenda molestiae, laudantium incidunt, obcaecati asperiores suscipit error rerum! Sapiente, minus.";
 const posts = [];
 app.get("/", (req, res) => {
-  res.render("home", { homeStartingContent: homeStartingContent });
+  res.render("home", {
+    homeStartingContent: homeStartingContent,
+    posts: posts,
+  });
 });
 
 app.get("/about", (req, res) => {
@@ -39,7 +42,6 @@ app.post("/compose", (req, res) => {
   post.title = req.body.postTitle;
   post.body = req.body.postBody;
   posts.push(post);
-  console.log(posts);
   res.redirect("/");
 });
 
